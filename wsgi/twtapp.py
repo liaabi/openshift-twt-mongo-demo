1b9ff5d6d9cb6147da67c822fefa06258977bf8f
Ligia Arghir <ligia.arghir@gmail.com>
2014-05-22 08:25:45 +0200

    changed connection

--- wsgi/twtapp.py
+++ wsgi/twtapp.py
@@ -9,11 +9,11 @@ import pymongo
 bottle.debug(True)
 
 #try this again
-mongo_con = pymongo.ReplicaSetConnection("os.environ['OPENSHIFT_MONGODB_DB_HOST1']:int(os.environ['OPENSHIFT_MONGODB_DB_PORT1']), \
-                                        os.environ['OPENSHIFT_MONGODB_DB_HOST2']:int(os.environ['OPENSHIFT_MONGODB_DB_PORT2']), \
-                                        os.environ['OPENSHIFT_MONGODB_DB_HOST3']:int(os.environ['OPENSHIFT_MONGODB_DB_PORT3'])")
+mongo_client = pymongo.MongoReplicaSetClient("mongodb://" + os.environ['OPENSHIFT_MONGODB_DB_HOST1'] + ":" + os.environ['OPENSHIFT_MONGODB_DB_PORT1'] + \
+                                          os.environ['OPENSHIFT_MONGODB_DB_HOST2'] + ":" + os.environ['OPENSHIFT_MONGODB_DB_PORT2'] + \
+                                          os.environ['OPENSHIFT_MONGODB_DB_HOST3'] + ":" + os.environ['OPENSHIFT_MONGODB_DB_PORT3'], replicaSet='rs0')
 
-mongo_db = mongo_con(os.environ['OPENSHIFT_APP_NAME'])
+mongo_db = mongo_client [os.environ['OPENSHIFT_APP_NAME']]
 mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
                       os.environ['OPENSHIFT_MONGODB_DB_PASSWORD'])
 
